67392d774ffe793b39e7ddf6aa1d081c1a7dccd9
chunchill <jasper.qiu@guanshantech.com>
2017-05-26 00:12:12 +0800

    add the item

--- apriori.py
+++ apriori.py
@@ -112,15 +112,25 @@ def runApriori(data_iter, minSupport, minConfidence):
                                            confidence))
     return toRetItems, toRetRules
 
+def decode_tuple_zh(item):
+    result = ''
+    for i in item:
+        if result is not '':
+            result = result + ',' + i
+        else:
+            result = result + i
+    result.rstrip(',')
+    return result
 
 def printResults(items, rules):
     """prints the generated itemsets sorted by support and the confidence rules sorted by confidence"""
     for item, support in sorted(items, key=lambda (item, support): support):
-        print "item: %s , %.3f" % (str(item), support)
+        print type(item)
+        print "item: %s , %.3f" % (decode_tuple_zh(item), support)
     print "\n------------------------ RULES:"
     for rule, confidence in sorted(rules, key=lambda (rule, confidence): confidence):
         pre, post = rule
-        print "Rule: %s ==> %s , %.3f" % (str(pre), str(post), confidence)
+        print "Rule: %s ==> %s , %.3f" % (decode_tuple_zh(pre), decode_tuple_zh(post), confidence)
 
 
 def dataFromFile(fname):
